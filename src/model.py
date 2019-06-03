4472fdb928a7ade450e5d82d8265749b143c3633
Louis Groux <lgroux@arista.com>
2019-06-03 18:53:47 +0100

    Submission number 16

--- src/model.py
+++ src/model.py
@@ -112,13 +112,13 @@ class KaggleModel(object):
         self._train, self._test = (all_data[:len(self._train)], all_data[len(self._train):])
 
     def model(self):
-        # self._model = Lasso(alpha=0.0005)
-        self._model = lgb.LGBMRegressor(objective='regression',num_leaves=5,
-                              learning_rate=0.05, n_estimators=720,
-                              max_bin = 55, bagging_fraction = 0.8,
-                              bagging_freq = 5, feature_fraction = 0.2319,
-                              feature_fraction_seed=9, bagging_seed=9,
-                              min_data_in_leaf =6, min_sum_hessian_in_leaf = 11)
+        self._model = Lasso(alpha=0.0005)
+        # self._model = lgb.LGBMRegressor(objective='regression',num_leaves=5,
+        #                       learning_rate=0.05, n_estimators=720,
+        #                       max_bin = 55, bagging_fraction = 0.8,
+        #                       bagging_freq = 5, feature_fraction = 0.2319,
+        #                       feature_fraction_seed=9, bagging_seed=9,
+        #                       min_data_in_leaf =6, min_sum_hessian_in_leaf = 11)
 
     def test(self):
         x = self._train
